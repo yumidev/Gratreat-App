9e4c7511d0347da3c59693c8fcde65510400e32c
Yumi Ko <yumi.ko.cheers@gmail.com>
2022-02-09 00:05:00 +0900

    update mock data and add local storage util

---
+++ src/utils/localStorage.js
@@ -0,0 +1,9 @@
+export const setLSdata = (dataObj) => {
+  const dataStr = JSON.stringify(dataObj);
+  window.localStorage.setItem('gratreat', dataStr);
+}
+export const getLSdata = () => {
+  const rawData = window.localStorage.getItem('gratreat')
+  const dataObj = JSON.parse(rawData);
+  return dataObj;
+}
